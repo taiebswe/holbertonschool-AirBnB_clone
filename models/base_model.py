39394785e1d5c1ea82f5e3b285be22149802af82
Taieb Chaabini <taieb.chaabini@holbertonschool.com>
2022-10-13 04:14:47 -0700

    Test

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,20 @@
 from datetime import datetime
 import uuid
+import builtins
+
 
 class BaseModel:
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if "__class__" != k:
+                    if k in ("created_at", "updated_at"):
+                        setattr(self, k, datetime.strptime(v,
+                                "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, k, v)
+            return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
